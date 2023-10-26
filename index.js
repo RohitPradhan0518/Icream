bfc65c3bf2dd4464ca1f85e498507b838d1e5943
Rohitpradhan <Rohitpradhan0518@gmail.com>
2023-10-26 16:02:18 -0500

    update

---
+++ index.js
@@ -0,0 +1,29 @@
+/*
+let baseprice = 0;
+let taxAmount = 0;
+let totalDue = 0;
+const TAX_RATE = 1.0828;
+
+
+function submitOrder(
+    scoopCount,
+     containerType, 
+     hasSprinkless, 
+     hasFudge, 
+     hasWip, 
+     hasCherry
+     ){*/
+
+     submitOrderButton.onclick = ()=>{
+        const sel
+        submitOrder(
+            scoopCount,
+            document.querySelector("input[name'containerType']):checked");
+            containerType, 
+            hasSprinkless, 
+            hasFudge, 
+            hasWip, 
+            hasCherry
+        )
+
+     }
